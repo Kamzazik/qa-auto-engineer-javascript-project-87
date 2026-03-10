750c707d4d8e713229fb323df5a69432c4730701
Никита <nikitanemcov34@gmail.com>
2026-03-10 11:04:56 +0300

    feat: add file arguments and format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,6 +9,15 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .helpOption('-h, --help', 'display help for command');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format <type>', 'output format') // Добавляем опцию для формата
+  .argument('<filepath1>', 'path to first configuration file') // Первый обязательный аргумент
+  .argument('<filepath2>', 'path to second configuration file') // Второй обязательный аргумент
+  .action((filepath1, filepath2, options) => {
+    // Здесь будет логика обработки файлов
+    console.log('filepath1:', filepath1);
+    console.log('filepath2:', filepath2);
+    console.log('format:', options.format); // Выводим выбранный формат
+  });
 
 program.parse(process.argv);
