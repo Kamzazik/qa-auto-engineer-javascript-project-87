abacf3ed59589144cd5849db7d14738beca721d6
Никита <nikitanemcov34@gmail.com>
2026-03-10 10:57:21 +0300

    feat: initialize project with commander.js CLI

---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import genDiff from '../src/index.js';
+
+const program = new Command();
+
+program
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .helpOption('-h, --help', 'display help for command');
+
+program.parse(process.argv);
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// src/index.js
+
+/**
+ * Главная функция, которая сравнивает два файла конфигурации
+ * @param {string} filepath1 - путь к первому файлу
+ * @param {string} filepath2 - путь ко второму файлу
+ * @returns {string} - строка с различиями
+ */
+export default (filepath1, filepath2) => {
+  // Пока просто заглушка
+  return 'Difference will be here';
+};
