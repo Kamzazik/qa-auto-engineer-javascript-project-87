024974a2dc9940d76e162bb3ce1967c31ec8665d
Никита <nikitanemcov34@gmail.com>
2026-03-20 11:45:13 +0300

    fix: ignore test files in ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ export default [
   js.configs.recommended,
   {
     files: ["**/*.js"],
+    ignores: ["__tests__/**", "coverage/**", "node_modules/**"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
@@ -14,23 +15,8 @@ export default [
       },
     },
     rules: {
-      // Отключаем все стилистические правила
-      "@stylistic/semi": "off",
-      "@stylistic/arrow-parens": "off",
-      "@stylistic/no-trailing-spaces": "off",
-      "@stylistic/eol-last": "off",
-      "@stylistic/quotes": "off",
-      "@stylistic/brace-style": "off",
-      "@stylistic/quote-props": "off",
-      "@stylistic/no-multi-spaces": "off",
-      "@stylistic/comma-dangle": "off",
-      
-      // Основные правила
       "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
       "semi": ["error", "always"],
     },
-  },
-  {
-    ignores: ["coverage/", "node_modules/"],
   },
 ];
