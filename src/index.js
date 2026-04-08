599325892ff5f3a2a803f1bd762aa2f11311783e
Никита <nikitanemcov34@gmail.com>
2026-04-08 09:26:50 +0300

    fix: reexport genDiff as default from index.js, move js-yaml to dependencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import { Command } from 'commander'
 import genDiff from './diff.js'
 
+// Реэкспорт genDiff как default для тестов
+export { default } from './diff.js'
+
 export function run() {
   const program = new Command()
 
