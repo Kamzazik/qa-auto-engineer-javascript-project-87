b70b4f32204e2325f1e6f64bf72552d34132cad0
Никита <nikitanemcov34@gmail.com>
2026-04-16 14:18:48 +0300

    fix: add proper eslint config to code directory

--- code/eslint.config.js
+++ code/eslint.config.js
@@ -4,7 +4,7 @@ import globals from 'globals'
 export default [
   js.configs.recommended,
   {
-    files: ['**/*.{js,mjs,cjs}'],
+    files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 2020,
       sourceType: 'module',
