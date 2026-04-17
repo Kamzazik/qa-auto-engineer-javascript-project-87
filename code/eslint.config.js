e745791c59fce1a873f6b59e699fd5486df55f7f
Никита <nikitanemcov34@gmail.com>
2026-04-17 12:25:52 +0300

    fix: upgrade @stylistic/eslint-plugin to ^5.3.1 as mentor requested

--- code/eslint.config.js
+++ code/eslint.config.js
@@ -1,27 +1,16 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+import pluginJest from 'eslint-plugin-jest'
 
-export default [
-  js.configs.recommended,
+export default defineConfig([
+  stylistic.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
   {
-    files: ['**/*.js'],
     languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-    rules: {
-      'semi': ['error', 'never'],
-      'no-trailing-spaces': 'error',
-      'eol-last': ['error', 'always'],
-      'arrow-parens': ['error', 'as-needed'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'brace-style': ['error', '1tbs'],
-      'quotes': ['error', 'single'],
-      'indent': ['error', 2],
+      globals: pluginJest.environments.globals.globals,
     },
   },
-]
+])
