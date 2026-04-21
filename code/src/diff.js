2a60999e01cbc2393545187ed07110abdd9523f6
Никита <nikitanemcov34@gmail.com>
2026-04-21 09:52:45 +0300

    refactor: separate library function from CLI

--- code/src/diff.js
+++ code/src/diff.js
@@ -7,10 +7,10 @@ import getFormatter from './formatters/index.js'
 export default function genDiff(filepath1, filepath2, formatName = 'stylish') {
   const data1Raw = fs.readFileSync(filepath1, 'utf-8')
   const data2Raw = fs.readFileSync(filepath2, 'utf-8')
-  
+
   const ext1 = path.extname(filepath1).toLowerCase().slice(1)
   const ext2 = path.extname(filepath2).toLowerCase().slice(1)
-  
+
   const data1 = parse(data1Raw, ext1)
   const data2 = parse(data2Raw, ext2)
 
