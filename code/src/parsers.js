a4ce9f757f795f2ac696209e735a184292a31dd3
Никита <nikitanemcov34@gmail.com>
2026-04-22 10:30:46 +0300

    style: fix linting issues (add newlines at end of files)

--- code/src/parsers.js
+++ code/src/parsers.js
@@ -1,9 +1,9 @@
 import yaml from 'js-yaml'
 
 const parsers = {
-  json: (data) => JSON.parse(data),
-  yaml: (data) => yaml.load(data),
-  yml: (data) => yaml.load(data),
+  json: data => JSON.parse(data),
+  yaml: data => yaml.load(data),
+  yml: data => yaml.load(data),
 }
 
 export default function parse(data, format) {
